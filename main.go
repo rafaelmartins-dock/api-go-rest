d72dfe861c5a10dbaf59522d2f7afe654df74103
Rafael Martins <rafael.martins@dock.tech>
2022-05-06 10:37:41 -0300

    api-go-rest

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/rafaelmartins-dock/go-rest-api/database"
+	"github.com/rafaelmartins-dock/go-rest-api/models"
+	"github.com/rafaelmartins-dock/go-rest-api/routes"
+)
+
+func main() {
+	models.Personalidades = []models.Personalidade{
+		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
+		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
+	}
+
+	database.ConectaComBancoDeDados()
+	fmt.Println("Iniciando o servidor Rest com Go")
+	routes.HandleRequest()
+}
